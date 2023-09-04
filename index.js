17f0e5b385799238191991ba404751905a559fe4
macclintx <macclintx@gmail.com>
2023-09-05 00:05:18 +0300

    changes to js functionability

--- index.js
+++ index.js
@@ -1,9 +1,21 @@
 
+let currentInput = document.getElementById("inp").value
+
+
+
+
 document.addEventListener('click', function(e){
     let elID=e.target.id;
     console.log(elID)
-    if(elID == "bracket-l"){
-        document.getElementById("inp").value="(" ;
-    }
- 
+
+        if(elID == "bracket-l")
+            currentInput+= "("
+            currentInput.innerHTML= "("
+        if(elID == "bracket-r")
+            currentInput += ")"
+            document.getElementById("inp").innerHTML=")"
+        if(elID = "")
+        if(elID == "equals")
+            currentInput = ""
+    console.log(currentInput)
 })
